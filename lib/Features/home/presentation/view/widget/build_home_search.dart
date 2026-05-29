f4bd764f99a1f9e1e5a101f82ccc33bf43ea5d41
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2026-05-29 10:45:58 +0200

    Create Search Screen

--- lib/Features/home/presentation/view/widget/build_home_search.dart
+++ lib/Features/home/presentation/view/widget/build_home_search.dart
@@ -1,36 +1,46 @@
 import 'package:flutter/material.dart';
 
+import '../../../../search/presentation/view/search_screen.dart';
+
 class BuildHomeSearch extends StatelessWidget {
   const BuildHomeSearch({super.key});
 
   @override
   Widget build(BuildContext context) {
     return
-      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: const Color(0xff3F80FF))
-        ),
-        child: Row(
-          children: [
-            const Icon(Icons.search, color: Colors.black),
-            const SizedBox(width: 8),
-             Expanded(
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "What are you looking for ?",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: InputBorder.none,
+      InkWell(
+        onTap: ()
+        {
+          showSearch(context: context, delegate: SearchScreen());
+        },
+        child: AbsorbPointer(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: const Color(0xff3F80FF))
+            ),
+            child: Row(
+              children: [
+                const Icon(Icons.search, color: Colors.black),
+                const SizedBox(width: 8),
+                 Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "What are you looking for ?",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                    ),
+                  ),
                 ),
-              ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.tune,size: 34, color: Color(0xff3F80FF)),
+                ),
+              ],
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.tune,size: 34, color: Color(0xff3F80FF)),
-            ),
-          ],
+          ),
         ),
       );
   }
