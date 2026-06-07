263d296ff377033824ab013e037f27c933789cc5
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2026-06-07 09:57:31 +0200

    Modify Favorite Screen

---
+++ lib/Features/favorite/domain/use_case/remove_favorite_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:marketi/Core/base_use_case/base_use_case.dart';
+import 'package:marketi/Core/error/failure.dart';
+import 'package:marketi/Features/favorite/data/model/add_or_remove_favorite_model.dart';
+import 'package:marketi/Features/favorite/domain/repo/favorite_repo.dart';
+
+class RemoveFavoriteUseCase extends BaseUseCase<AddOrRemoveFavoriteModel, String>{
+  final FavoriteRepo favoriteRepo;
+
+  RemoveFavoriteUseCase(this.favoriteRepo);
+  @override
+  Future<Either<Failure, AddOrRemoveFavoriteModel>> call([String? id])async{
+    return await favoriteRepo.removeFavorite(productId: id!);
+  }
+
+}
