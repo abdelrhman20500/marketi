a78834c4c3564f50177a4190021d0e686de9351d
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2026-06-10 12:05:10 +0200

    Modify Endpoint For Api

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marketi/Core/Api/dio_consumer.dart';
 import 'package:marketi/Features/layout/presentation/view/layout_screen.dart';
-import 'package:marketi/Features/menu/data/data_source/user_remote_data_source.dart';
-import 'package:marketi/Features/menu/data/repo/user_repo_impl.dart';
-import 'package:marketi/Features/menu/domain/use_case/user_use_case.dart';
-import 'package:marketi/Features/menu/presentation/view_manager/user_cubit.dart';
 import 'Core/Api/simple_bloc_observer.dart';
 import 'Core/cached/shared_pref.dart';
+import 'Features/cart/data/data_source/cart_remote_data_source.dart';
+import 'Features/cart/data/repo/cart_repo_impl.dart';
+import 'Features/cart/domain/use_case/add_cart_use_case.dart';
+import 'Features/cart/domain/use_case/cart_use_case.dart';
+import 'Features/cart/domain/use_case/remove_cart_use_case.dart';
 import 'Features/cart/presentation/view_manager/cart_cubit/cart_cubit.dart';
+import 'Features/favorite/data/data_source/favorite_remote_data_source.dart';
+import 'Features/favorite/data/repo/favorite_repo_impl.dart';
+import 'Features/favorite/domain/use_case/add_to_favorite_use_case.dart';
+import 'Features/favorite/domain/use_case/favorite_use_case.dart';
+import 'Features/favorite/domain/use_case/remove_favorite_use_case.dart';
+import 'Features/favorite/presentation/ view_manager/favorite_cubit/favorite_cubit.dart';
 import 'Features/splash/presentation/view/splash_screen.dart';
 
 
